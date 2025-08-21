63fafd8120cea6bd81d46472dd552c0b09bdb626
Your Name <anything>
2025-08-21 21:06:14 +0000

    app.py changed

--- .ipynb_checkpoints/post-checkpoint.lua
+++ .ipynb_checkpoints/post-checkpoint.lua
@@ -1,3 +1,3 @@
 wrk.method = "POST"
-wrk.body = '{"longitude": -121.89, "latitude": 37.34, "housing_median_age": 29.0, "total_bedrooms": 150.0, "population": 400.0, "households": 140.0, "median_income": 6.1234, "median_house_value": 300500.0, "ocean_proximity": "INLAND", "total_rooms": 850.0}'
+wrk.body = '{"longitude": -121.89, "latitude": 37.34, "housing_median_age": 29.0, , "total_rooms": 850.0, "total_bedrooms": 150.0, "population": 400.0, "households": 140.0, "median_income": 6.1234, "high_income": 1}'
 wrk.headers["Content-Type"] = "application/json"
--- deployment/.ipynb_checkpoints/app-checkpoint.py
+++ deployment/.ipynb_checkpoints/app-checkpoint.py
@@ -52,13 +52,13 @@ class Schema(BaseModel):
     longitude: float
     latitude: float 
     housing_median_age: float
+    total_rooms: float
     total_bedrooms: float
     population: float
     households: float 
     median_income: float
-    median_house_value: float
-    ocean_proximity: str
-    total_rooms: float
+    high_income: float
+    
 
 app_state = {"is_ready": False, "is_alive": True}
 
