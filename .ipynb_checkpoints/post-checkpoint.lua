56e37a8137b464fd0dd727154ad43a3e61b6e02d
Your Name <anything>
2025-08-21 21:16:41 +0000

    post.lua fixed

--- .ipynb_checkpoints/post-checkpoint.lua
+++ .ipynb_checkpoints/post-checkpoint.lua
@@ -1,3 +1,3 @@
 wrk.method = "POST"
-wrk.body = '{"longitude": -121.89, "latitude": 37.34, "housing_median_age": 29.0, , "total_rooms": 850.0, "total_bedrooms": 150.0, "population": 400.0, "households": 140.0, "median_income": 6.1234, "high_income": 1}'
+wrk.body   = '{"longitude": -121.89, "latitude": 37.34, "housing_median_age": 29.0, "total_rooms": 850.0, "total_bedrooms": 150.0, "population": 400.0, "households": 140.0, "median_income": 6.1234, "high_income": 1}'
 wrk.headers["Content-Type"] = "application/json"
